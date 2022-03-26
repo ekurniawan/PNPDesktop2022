032d9ac57c6af19c2ecfa57e64f4835c60f08793
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-03-26 11:40:13 +0700

    menambahkan dbconnection

--- POSApplication.FrontEnd/Repository/DapperContext.cs
+++ POSApplication.FrontEnd/Repository/DapperContext.cs
@@ -5,33 +5,99 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Common;
+using Dapper;
 
 namespace POSApplication.FrontEnd.Repository
 {
     public class DapperContext : IDapperContext
     {
-        public IDbConnection db => throw new NotImplementedException();
+        private IDbConnection _db;
+        private readonly string _connStr;
+        private readonly string _providerName;
+
+        public IDbConnection db
+        {
+            get { return _db ?? (_db=GetOpenConnection(_connStr,_providerName)); }
+        }
 
         public IDbTransaction transaction => throw new NotImplementedException();
+
+        public DapperContext()
+        {
+            _connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            _providerName = ConfigurationManager.ConnectionStrings["DefaultConnection"].ProviderName;
+
+            if (_db == null)
+            {
+                _db = GetOpenConnection(_connStr, _providerName);
+            }
+        }
+
+        private IDbConnection GetOpenConnection(string connStr,string providerName)
+        {
+            DbConnection conn = null;
+            try
+            {
+                DbProviderFactory provider = DbProviderFactories.GetFactory(providerName);
+                conn = provider.CreateConnection();
+                conn.ConnectionString = connStr;
+                conn.Open();
+            }
+            catch
+            {}
+
+            return conn;
+        }
 
         public void BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
         {
             throw new NotImplementedException();
         }
-
         public void Commit()
         {
             throw new NotImplementedException();
         }
-
-        public void Dispose()
+        public void Rollback()
         {
             throw new NotImplementedException();
         }
 
+
+
+        public void Dispose()
+        {
+            if(_db != null)
+            {
+                try
+                {
+                    if(_db.State != ConnectionState.Closed)
+                    {
+                        _db.Close();
+                    }
+                }
+                finally
+                {
+                    _db.Dispose();
+                }
+            }
+            GC.SuppressFinalize(this);
+        }
+
         public bool ExecSQL(string sql)
         {
-            throw new NotImplementedException();
+            var result = false;
+            try
+            {
+                _db.Execute(sql);
+                result = true;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return result;
         }
 
         public string GetGUID()
@@ -41,12 +107,18 @@ namespace POSApplication.FrontEnd.Repository
 
         public bool IsOpenConnection()
         {
-            throw new NotImplementedException();
+            var isConnected = false;
+            try
+            {
+                using(var conn = GetOpenConnection(_connStr,_providerName))
+                {
+                    isConnected = conn.State == ConnectionState.Open;
+                }
+            }
+            catch { }
+            return isConnected;
         }
 
-        public void Rollback()
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
