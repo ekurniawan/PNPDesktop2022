19e43dcf1b64bb0ce8cbdcd6ef905eb3124925bd
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-04-09 09:10:38 +0700

    menambahkan select data barang

---
+++ POSApplication.FrontEnd/FormBarang.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace POSApplication.FrontEnd
+{
+    partial class FormBarang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvBarang = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBarang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBarang
+            // 
+            this.dgvBarang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBarang.Location = new System.Drawing.Point(21, 21);
+            this.dgvBarang.Name = "dgvBarang";
+            this.dgvBarang.RowHeadersWidth = 51;
+            this.dgvBarang.RowTemplate.Height = 29;
+            this.dgvBarang.Size = new System.Drawing.Size(748, 188);
+            this.dgvBarang.TabIndex = 0;
+            // 
+            // FormBarang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvBarang);
+            this.Name = "FormBarang";
+            this.Text = "FormBarang";
+            this.Load += new System.EventHandler(this.FormBarang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBarang)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dgvBarang;
+    }
+}
---
+++ POSApplication.FrontEnd/FormBarang.cs
@@ -0,0 +1,30 @@
+﻿using POSApplication.FrontEnd.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSApplication.FrontEnd
+{
+    public partial class FormBarang : Form
+    {
+        private BarangDAL barangDAL;
+
+        public FormBarang()
+        {
+            InitializeComponent();
+            barangDAL = new BarangDAL();
+        }
+
+        private void FormBarang_Load(object sender, EventArgs e)
+        {
+            var results = barangDAL.GetAll();
+            dgvBarang.DataSource = results.ToList();
+        }
+    }
+}
