3edf1eb35e475ceb8d15a0281fba94eec8e5debc
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-05-28 10:22:58 +0700

    menambahkan supplier dal

---
+++ POSApplication.FrontEnd/DAL/SupplierDAL.cs
@@ -0,0 +1,31 @@
+﻿using POSApplication.FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSApplication.FrontEnd.DAL
+{
+    public class SupplierDAL
+    {
+        private ApplicationDbContext _dbContext;
+        public SupplierDAL()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Supplier> GetAll()
+        {
+            var suppliers = _dbContext.Suppliers.OrderBy(s => s.Nama);
+            return suppliers;
+        }
+
+        public IEnumerable<Supplier> GetByNama(string nama)
+        {
+            var results = _dbContext.Suppliers.Where(s => s.Nama.Contains(nama))
+                .OrderBy(s => s.Nama);
+            return results;
+        }
+    }
+}
